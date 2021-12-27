760eb0374796451bc77fa9c2c0d998d4e2d6af89
Lou Minxi <Lou Minxi@DESKTOP-M69RIDK>
2021-12-27 17:11:16 +0000

    Kata4 Version 2

--- src/kata04/main/Kata04.java
+++ src/kata04/main/Kata04.java
@@ -1,20 +1,34 @@
 package kata04.main;
 
-import kata04.view.MailListReader;
-import kata04.view.HistogramDisplay;
-import kata04.view.MailHistogramBuilder;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import kata04.model.Histogram;
+import kata04.model.Mail;
+import kata04.view.*;
 
 public class Kata04 {
 
     public static void main(String[] args) throws IOException {
         String nameFile = "C:/Users/Lou Minxi/Documents/NetBeansProjects/Kata04_44/email.txt";
-        MailListReader leer = new MailListReader();
-
-        MailHistogramBuilder mostrar = new MailHistogramBuilder();
-
-        HistogramDisplay histo = new HistogramDisplay(mostrar.build(leer.read(nameFile)));
-        histo.execute();
+        input(nameFile);
     }
+
+    public static void execute() {
+        MailHistogramBuilder mostrar = new MailHistogramBuilder();
+        MailListReader leer = new MailListReader();
+    }
+
+    public static void input(String nameFile) throws FileNotFoundException, IOException {
+        process(MailListReader.read(nameFile));
+    }
+
+    public static void process(List<Mail> mailList) {
+        output(MailHistogramBuilder.build(mailList));
+    }
+
+    public static void output(Histogram<String> histogram) {
+        new HistogramDisplay(histogram).execute();
+    }
+
 }
